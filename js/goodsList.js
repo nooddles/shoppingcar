7d521ec129e294c96b6ea8a93a4b6d2d7062f0f7
nooddles <961488570@qq.com>
2020-11-16 17:51:16 +0800

    运行git 版本1.0

---
+++ js/goodsList.js
@@ -0,0 +1,65 @@
+$(function (){
+
+  // 获取商品列表数据
+  $.ajax({
+    url: './data/goods.json',
+    type: 'get',
+    dataType: 'json',
+    success: function (json){
+      // console.log(json)
+      var goodsStr = ''
+      $.each(json,function (index,item){
+        goodsStr += `<div class="goods">
+          <img src="${item.imgurl}" alt="">
+          <p>${item.price}</p>
+          <h3>${item.title}</h3>
+          <div code="${item.code}">加入购物车</div>
+        </div>`
+      })
+      $('.content').html(goodsStr)
+    }
+  })
+
+  // 点击加入购物车
+  $('.content').on('click','.goods div',function (){
+    // 获取当前点击商品的编号
+    var code = $(this).attr('code')
+    
+    // localStorage  key = value
+              //  goods = [{code:'abc1',num:1},{code:'abc2',num:2}]
+    // 判断本地存储是否有数据
+    if (localStorage.getItem('goods')) {
+      var goodsArr = JSON.parse( localStorage.getItem('goods') )
+    } else {
+      var goodsArr = []
+    }
+
+    var hasGoods = false
+
+    if (goodsArr.length > 0) {
+      // 判断当前选中商品是否在购物车中
+      $.each(goodsArr,function (index,item){
+        console.log(index)
+        console.log(item)
+        if (item.code === code) {// 商品存在购物车中，数量+1
+          item.num++
+          hasGoods = true
+          return false
+        }
+      })
+    }
+
+    // 如果购物车没有当前选中的商品，添加一条数据
+    if (!hasGoods) {
+      // var objStr = JSON.stringify({code:code,num:1})
+      goodsArr.push({code:code,num:1})
+    }
+
+    // 更新本地存储的数据
+    localStorage.setItem('goods',JSON.stringify(goodsArr))
+
+    alert('添加购物车成功')
+
+  })
+
+})
